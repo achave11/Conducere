3aa0076692865d03cece0f239fc94d1794fe75f6
Ryan Teves <rteves@ucsc.edu>
2017-03-24 11:22:36 -0700

    ran trial 1 and 2

--- data/echonest.py
+++ data/echonest.py
@@ -6,8 +6,9 @@
 
 import time
 import os
-from pyechonest import config, artist, song, track, util
-from util import contains_all_attributes, ATTRIBUTES
+import spotipy as sp
+#from pyechonest import config, artist, song, track, util
+#from util import contains_all_attributes, ATTRIBUTES
 
 # ==========================================
 # TODO: Make sure the following variables is set
@@ -26,41 +27,36 @@ def get_playlist_track_analysis(playlist_tracks):
   Return:
     A list of track features for songs in the specified playlist
   """
-  set_api_key()
+  #set_api_key()
   analysis = []
   for i in range(len(playlist_tracks)):
     if i % 10 == 0:
       print 'On %dth track' % i
     
     print playlist_tracks[i]
-    try:
-      next_track = track.track_from_id(playlist_tracks[i])
-      print next_track
-      print '---'
-    except util.EchoNestAPIError:
-      print 'Error extracting track profile: %s' % playlist_tracks[i]
-      pass
+#    try:
+    next_track = sp.audio_features(playlist_tracks[i])
+    for next_tracks in next_track:
+	analysis = sp._get(next_tracks['analysis_url'])
+    analysis.append(next_track)
+#      print next_track
+#      print '---'
+#    except Exception:
+#      print 'Error extracting track profile: %s' % playlist_tracks[i]
+#      pass
 
-    try:
-      if not contains_all_attributes(next_track):
+#    try:
+#      if not contains_all_attributes(next_track):
         # Try to get analysis, if missing attributes
-        next_track = next_track.get_analysis()
-      analysis.append(next_track)
-    except Exception:
-      print 'Error processing track: %s' % next_track
-      pass 
+#        next_track = next_track.audio_features()
+#      analysis.append(next_track)
+#    except Exception:
+#      print 'Error processing track: %s' % next_track
+#      pass 
 
     time.sleep(3) # limited to 20 access/s
     if i % 20 == 0 and i > 0:
       print "Completed %d tracks" % i
   return analysis
-
-def set_api_key():
-  """
-  Description:
-    Authenticate access to the Echonest API, using the
-    environment variable ECHO_NEST_API_KEY
-  """
-  config.ECHO_NEST_API_KEY=os.environ['ECHO_NEST_API_KEY']
 
 
